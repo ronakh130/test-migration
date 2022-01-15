7b805e5d61adec53d88db13e8ab879cc95faccf2
Ronak Hirpara <ronakh130@gmail.com>
2022-01-15 15:56:21 -0500

    added aceEditor

--- src/containers/BodyComponents/CodeContainer/CodeContainer.jsx
+++ src/containers/BodyComponents/CodeContainer/CodeContainer.jsx
@@ -1,9 +1,19 @@
 import './CodeContainer.css';
+import AceEditor from "react-ace";
 
 const CodeContainer = () => {
   return (
     <div className="codeContainer">
-      Code Editor
+      <AceEditor
+        mode="javascript"
+        theme="monokai"
+        width="100%"
+        height="100%"
+        name="Code_div"
+        readOnly={false}
+        fontSize={16}
+        tabSize={2}
+      />
     </div>
   )
 }
